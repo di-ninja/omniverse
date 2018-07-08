dbbeb47b84dbe778acce9a4380db0f4607e28330
takion <jo@surikat.pro>
2018-07-08 10:03:47 +0200

    remove decorator-performance + fix vulnerabilities + lint

--- src/omniverse.js
+++ src/omniverse.js
@@ -1,56 +1,55 @@
 export container from 'di-ninja'
 
 export {
-	
-	instanceOf,
-	Interface,
-	
-	InterfacePrototype,
-	InterfaceClass,
-	
+
+  instanceOf,
+  Interface,
+
+  InterfacePrototype,
+  InterfaceClass,
+
 } from 'interface-prototype'
 
 export mixin from './mixin'
 
 export {
-	
-	mix,
-	Mixin,
-	
+
+  mix,
+  Mixin,
+
 } from 'mixwith'
 
 
 
 export {
-	//For Properties and Methods
-	readonly,
-	nonconfigurable,
-	decorate,
-	extendDescriptor,
+  //For Properties and Methods
+  readonly,
+  nonconfigurable,
+  decorate,
+  extendDescriptor,
 
-	//For Properties
-	nonenumerable,
-	lazyInitialize,
-	
-	//For Classes and Methods
-	autobind,
+  //For Properties
+  nonenumerable,
+  lazyInitialize,
 
-	//For Methods
-	deprecate,
-	suppressWarnings,
-	enumerable,
-	override,
-	time,
-	profile,
+  //For Classes and Methods
+  autobind,
+
+  //For Methods
+  deprecate,
+  suppressWarnings,
+  enumerable,
+  override,
+  time,
+  profile,
 
 
-	//Helpers
-	applyDecorators,
-	
+  //Helpers
+  applyDecorators,
+
 } from 'core-decorators'
 
 export debug from 'decorator-debug'
-export { performance } from 'decorator-performance'
 
 export memoize from './memoize'
 
@@ -60,7 +59,7 @@ export condition from './condition'
 
 
 import {
-	InterfacePrototype
+  InterfacePrototype
 } from 'interface-prototype'
 import container from 'di-ninja'
 container.setInterfacePrototypeDefault(InterfacePrototype);
